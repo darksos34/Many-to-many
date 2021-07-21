d50bd8c67b9513ef6cf1e92ad7c71cf1bc562b8f
darksos34 <j.hamwijk34@gmail.com>
2021-07-21 14:08:00 +0200

    Clean up

--- Many-to-many/src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO tags(id,name,posts) VALUES ('1', 'Henk','Good');
-
-INSERT INTO posts(id,title,description,content,postedAt,lastUpdatedAt,tags) VALUES ('10','Good','Wat een mooi beeld', 'sukkel','20-07-2021','21-07-2021','1')
-

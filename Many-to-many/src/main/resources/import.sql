f921a26c31dafe40da3546e7f5dd70f925a9682b
darksos34 <j.hamwijk34@gmail.com>
2021-07-21 14:03:06 +0200

    Initial commit

---
+++ Many-to-many/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tags(id,name,posts) VALUES ('1', 'Henk','Good');
+
+INSERT INTO posts(id,title,description,content,postedAt,lastUpdatedAt,tags) VALUES ('10','Good','Wat een mooi beeld', 'sukkel','20-07-2021','21-07-2021','1')
+
